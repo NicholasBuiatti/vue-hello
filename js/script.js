e4f0340f6a738619294aa24a8c78ea6db4238c5d
NicholasBuiatti <nic.buiatti@gmail.com>
2024-05-10 16:18:19 +0200

    esperiemnti vari

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: `Benvenuto in Vue! l'inizio di un nuovo mondo`,
+            nigthMode: '',
+            txtColorSize: 'text-primary text-center',
+            pic: './img/img-1.jpg',
+            sizeImg: 'w-75',
+            position: 'text-center'
         }
+    },
+    methods: {
+        switchMode() {
+            if (this.nigthMode == "") {
+                this.nigthMode = "vh-100 bg-dark";
+                this.txtColorSize = 'text-danger text-center';
+            } else {
+                this.nigthMode = "";
+                this.txtColorSize = 'text-primary text-center';
+            }
+        },
+        
     }
 }).mount('#app')
